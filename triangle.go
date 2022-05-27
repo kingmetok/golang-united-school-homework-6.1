5cfb92bc9aff08b4e324abe22b0d946f03e18407
Mykhailo Pinchuk <kingmetok@gmail.com>
2022-05-27 16:18:58 +0400

    Implemented solution

--- triangle.go
+++ triangle.go
@@ -1,6 +1,18 @@
 package golang_united_school_homework
 
+import "math"
+
+var squaredThreeDividedFour = math.Sqrt(3) / 4
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Side float64
+}
+
+func (t Triangle) CalcPerimeter() float64 {
+	return 3 * t.Side
+}
+
+func (t Triangle) CalcArea() float64 {
+	return squaredThreeDividedFour * math.Pow(t.Side, 2)
 }
